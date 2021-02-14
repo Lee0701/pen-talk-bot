f92087a9f7ddce826f037fc3f7d637dc527c63f9
Lee0701 <hgl1002@naver.com>
2021-02-14 18:56:47 +0900

    Add confucius command

---
+++ commands/confucius.js
@@ -0,0 +1,30 @@
+
+const fs = require('fs')
+const sf = require('sf')
+const confucius = fs.readFileSync('data/confucius.txt').toString().split('\n')
+const confuciusTop = confucius.shift()
+const confuciusRest = confucius.join('\n')
+
+module.exports = (msg, cmd, args, rest) => {
+    const leftWidth = 26
+    const rightWidth = 5
+    const width = 10
+    const totalWidth = leftWidth + width + rightWidth
+    const height = 6
+    const lines = rest
+            .match(new RegExp(`.{1,${Math.floor(width/2)}}`, 'g'))
+            .map((line, i, arr) => (i == arr.length-1) ? '| ' + line : '  ' + line)
+    lines.unshift('  ' + ' '.repeat(width) + '|')
+    lines.unshift('  ' + ' '.repeat(width-2) + '--')
+    lines.push('|__')
+    if(lines.length < height) lines.unshift(...new Array(height - lines.length).fill(''))
+    const topLines = (lines.length > height) ? lines.splice(0, lines.length - height) : []
+    const topMessageLines = topLines.map((line) => sf(confuciusTop, line))
+    const restMessage = sf(confuciusRest, ...lines).split('\n')
+    const text = padLines([...topMessageLines, ...restMessage], totalWidth).join('\n')
+    msg.channel.send('```' + text + '```')
+}
+
+const padLines = (lines, width) => lines.map((line) => padLine(line, width))
+const padLine = (line, width) => line + new Array(getPadWidth(line, width)).fill(' ').join('')
+const getPadWidth = (line, width) => Math.max(0, width - line.length - (line.match(/[^ -~]/g) || []).length)
--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const prefix = '/'
 
 const bot = new Discord.Client()
 bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`)
+    console.log(`Logged in as ${bot.user.tag}.`)
 })
 bot.on('message', (msg) => {
     if(msg.author.bot) return
     if(!msg.content) return
 
     const text = msg.content
+
+    if(text.startsWith('子曰')) commands['子曰'](msg, '子曰', text.slice(2).trim())
 
     if(text.substring(0, prefix.length) == prefix) {
         const command = text.substring(prefix.length, text.includes(' ') ? text.indexOf(' ') : text.length)
@@ -25,5 +27,8 @@ bot.login(process.env.BOT_TOKEN)
 
 const commands = {
     'furi': require('./commands/furi.js'),
-    'yomi': require('./commands/furi.js')
+    'yomi': require('./commands/furi.js'),
+    'confucius': require('./commands/confucius.js'),
+    'said': require('./commands/confucius.js'),
+    '子曰': require('./commands/confucius.js'),
 }
