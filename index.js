0d92d60b29e192ab7e737c8a9ae0f32f75f5f901
Lee0701 <hgl1002@naver.com>
2021-07-08 00:12:15 +0900

    Add storywriting function

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ bot.on('message', (msg) => {
     if(!msg.content) return
 
     const text = msg.content
+    const topic = msg.channel.topic
+
+    if(topic && topic.includes('@作譚')) {
+        const channelId = topic.substring(topic.indexOf('@作譚') + 3).split('\n')[0]
+        const toChannel = msg.channel.guild.channels.cache.get(channelId)
+        if(toChannel) {
+            const last = toChannel.lastMessage
+            if(last && last.content.length <= 2000 - text.length && last.editable) {
+                last.edit(last.content + text)
+            } else {
+                toChannel.send(text)
+            }
+        }
+    }
 
     if(text.startsWith('子曰')) commands['子曰'](msg, '子曰', [text.slice(2).trim()])
 
